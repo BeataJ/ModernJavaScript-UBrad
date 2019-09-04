f29c495ef7c54e3f147df5ebf98d0fdd1dc4fffd
beata <beata@emc2wave.com>
2019-09-04 16:52:12 -0700

    varable let

--- var.js
+++ var.js
@@ -1,23 +1,29 @@
-// var, let, const
-var name = 'Joe Doe';
+// // var, let, const
+// var name = 'Joe Doe';
+// console.log(name);
+// name = 'Steve Smith',
+// console.log(name);
+
+// // Init var 
+// var greeting;
+// console.log(greeting);
+// greeting = 'hi';
+// console.log(greeting);
+
+// // letters, numbers, _, $
+// // Can not start with number
+
+// // Multi word vars
+
+// // camel case
+// var firstName = 'John'; 
+// // Underscore case
+// var first_name = 'Sara'
+// // Pascal case
+// var FirstName = 'Tom';
+
+// Let
+let name = 'Joe Doe';
 console.log(name);
 name = 'Steve Smith',
 console.log(name);
-
-// Init var 
-var greeting;
-console.log(greeting);
-greeting = 'hi';
-console.log(greeting);
-
-// letters, numbers, _, $
-// Can not start with number
-
-// Multi word vars
-
-// camel case
-var firstName = 'John'; 
-// Underscore case
-var first_name = 'Sara'
-// Pascal case
-var FirstName = 'Tom';
