69dae54c6d1ba3824de5fde9889b37d4136d6c1f
beata <beata@emc2wave.com>
2019-09-04 16:49:53 -0700

    var nemes cases

--- var.js
+++ var.js
@@ -7,3 +7,17 @@ console.log(name);
 // Init var 
 var greeting;
 console.log(greeting);
+greeting = 'hi';
+console.log(greeting);
+
+// letters, numbers, _, $
+// Can not start with number
+
+// Multi word vars
+
+// camel case
+var firstName = 'John'; 
+// Underscore case
+var first_name = 'Sara'
+// Pascal case
+var FirstName = 'Tom';
