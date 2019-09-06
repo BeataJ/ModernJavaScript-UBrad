ed186f4bade08477af3f10ab9ff3fbd419ee973b
beata <beata@emc2wave.com>
2019-09-06 16:18:17 -0700

    templating

---
+++ js/template.js
@@ -0,0 +1,34 @@
+const tname = 'John';
+const tage = 3;
+const job = 'Web Developer';
+const city = 'Miami';
+let html;
+
+// Without template string (es5)
+html = '<ul><li>Name: ' + tname + ' </li><li>Age: ' + tage +  ' </li><li>Job: ' + job +' </li><li>City:  ' + city +  ' </li></ul>';
+
+html = '<ul>' +
+      '<li>Name: ' + tname + ' </li>' +
+      '<li>Age: ' + tage + ' </li>' +
+      '<li>Job: ' + job + ' </li>' +
+      '<li>City: ' + city + ' </li>' + 
+      '</ul>';
+
+function hello() {
+  return 'Hello';
+}      
+
+// Whit template string (es6)
+html = `
+  <ul>
+    <li>Name: ${tname}</li>
+    <li>Age: ${tage}</li>
+    <li>Job: ${job}</li>
+    <li>City: ${city}</li>
+    <li>${2 + 2}</li>
+    <li>${hello()}</li>
+    <li>${tage > 30 ? 'Over 30' : 'Under 30'}</li>
+  </ul>
+`
+
+document.body.innerHTML = html;
