6cbbc32680d549ab11908aabf6c1df7647a9665d
beata <beata@emc2wave.com>
2019-09-06 14:15:35 -0700

    number and math methods

---
+++ number_math_obj.js
@@ -0,0 +1,26 @@
+const n1 = 100;
+const n2 = 50;
+let v;
+
+// Simple math
+v = n1 + n2;
+v = n1 - n2;
+v = n1 * n2;
+v = n1 / n2;
+v = n1 % n2;
+
+v = Math.PI;
+v = Math.E;
+v = Math.round(4.49);
+v = Math.ceil(2.4);
+v = Math.floor(2.9);
+v = Math.sqrt(64);
+v = Math.abs(-3);
+v = Math.pow(5, 2);
+v = Math.min(2,1.7,-7,7.6,56,56.1,3,4);
+v = Math.max(2,1.7,-7,7.6,56,56.1,3,4);
+v = Math.random();
+
+v = Math.floor(Math.random() * 20 + 1);
+
+console.log(v);
