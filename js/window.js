e22cbcabb63438ae2b4cc4f812f624de5f71c421
beata <beata@emc2wave.com>
2019-09-13 16:10:52 -0700

    create window

---
+++ js/window.js
@@ -0,0 +1,50 @@
+// WINDOW METHODS / OBJECTS / PROPERTIES
+
+// console.log(123);
+
+// ALERT
+//  alert('Hi');
+
+// PROMPT
+// const input = prompt();
+// alert(input);
+
+// Confirm
+// if(confirm('Are you sure')) {
+//   console.log('Yes');
+// } else {
+//   console.log('No');
+// }
+
+let valW;
+
+// Outer height and width
+valW = window.outerHeight;
+valW = window.outerWidth;
+
+// Inner height and width
+valW = window.innerHeight;
+valW = window.innerWidth;
+
+// Scroll points
+valW = window.scrollY;
+
+// location object---------
+valW = window.location;
+valW = window.location.hostname;
+valW = window.location.port;
+valW = window.location.href;
+valW = window.location.search;
+
+// Redirect
+// window.location.href = 'http://cbc.ca';
+
+// Reload
+// window.location.reload();
+
+// History Object
+
+window.history.go(2);
+
+
+console.log(valW);
