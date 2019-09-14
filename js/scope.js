fa9379f066655a70f55026a984f1756172c9b070
beata <beata@emc2wave.com>
2019-09-13 17:26:52 -0700

    exercises scopes

---
+++ js/scope.js
@@ -0,0 +1,32 @@
+// GLOBAL SCOPE
+
+var a = 1;
+let b = 2;
+const c = 3;
+
+// function test1() {
+//   var a = 4;
+//   let b = 5;
+//   const c = 6;
+
+//   console.log('Func scope ', a, b, c )
+// }
+
+// test1();
+
+// if (true) {
+//   // Block scope
+//   var a = 4;
+//   let b = 5;
+//   const c = 6;
+
+//   console.log('Block scope: ', a, b, c);
+// }
+
+// for(var a = 0; a < 10; a++) {
+//   console.log(`loop: ${a}`);
+// }
+
+console.log("Global scope ", a, b, c);
+
+console.clear();
